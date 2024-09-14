9f501c39fbfbbc51ef688e65a6789ccc8c02def4
MuraliKrishnaPole95 <krishnapole90@gmail.com>
2024-09-14 08:39:28 +0300

    Made changes to assignment files

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -8,4 +8,18 @@ height = input()
 
 
 #code here
+weight = float(input("enter weight in kilograms: "))
+height = float(input("enter height in meters: "))
 
+bmi = weight / height**2
+
+print(f"Your Body Mass Index (BMI) is: {bmi:.2f}")
+
+if bmi < 18.5:
+    print("You are underweight.")
+elif 18.5 <= bmi < 24.9:
+    print("You have a normal weight.")
+elif 25 <= bmi < 29.9:
+    print("You are overweight.")
+else:
+    print("You are obese.")
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,15 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+number = input("enter a 4-digit number? ")
+
+if len(number) == 4 and number.isdigit():
+    digits = [int(digit) for digit in number]
+    total_sum = sum(digits)
+    
+    equation = ' + '.join(number)
+    
+    print(f"break it down: {equation} equals {total_sum}.")
+    print(f"sum of the digits in {number} is {total_sum}.")
+else:
+    print("wrong 4-digit number. Try again?")
